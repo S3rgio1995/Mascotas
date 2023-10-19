4a7be3d7e6a9fda7542925dc672b5f0c93dec3a0
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-10-19 08:27:46 -0500

    S4

--- app/src/main/java/com/example/mascotas/Gato.java
+++ app/src/main/java/com/example/mascotas/Gato.java
@@ -1,5 +1,12 @@
 package com.example.mascotas;
 
 public class Gato {
+String Nombre, colorpelaje, genero, raza;
+Double altura, peso;
+Integer edad;
+    public String gato (String nombre) {
+        Nombre = nombre;
+        return Nombre;
+    }
 
 }
