ee0c8a1a11b513282b8efdb5085f1e3b8d35bc91
S3rgio1995 <sergiolopezg.sl@gmail.com>
2023-10-20 06:34:05 -0500

    S4

--- app/src/main/java/com/example/mascotas/Gato.java
+++ app/src/main/java/com/example/mascotas/Gato.java
@@ -4,9 +4,14 @@ public class Gato {
 String Nombre, colorpelaje, genero, raza;
 Double altura, peso;
 Integer edad;
-    public String gato (String nombre) {
-        Nombre = nombre;
-        return Nombre;
-    }
 
+    public Gato(String nombre, String colorpelaje, String genero, String raza, Double altura, Double peso, Integer edad) {
+        Nombre = nombre;
+        this.colorpelaje = colorpelaje;
+        this.genero = genero;
+        this.raza = raza;
+        this.altura = altura;
+        this.peso = peso;
+        this.edad = edad;
+    }
 }
